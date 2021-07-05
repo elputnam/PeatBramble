daa3b1294541b3479cca0eafaa1c8ca563532ffc
elputnam <elputnam@inaction.ie>
2021-07-05 16:54:16 +0100

    initial commit

---
+++ sketch.js
@@ -0,0 +1,118 @@
+var tileCount;
+let actRandomSeed = 0;
+var col1;
+var col2;
+var col3;
+var col4;
+let particles = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+  col1 = color(70, 50, 100, 40);
+  col2 = color(0, 50, 100, 40);
+  col3 = color(200, 50, 100, 60);
+  col4 = color(300, 100, 100, 60);
+  
+  tileCount = random(10);
+  strokeCap(PROJECT);
+  
+}
+
+function draw() {
+  background(10, 1);
+  grid();
+  particles.push(new Element(createVector(mouseX, mouseY)));
+
+  for(let i = particles.length - 1; i >= 0; i--){
+    let p = particles[i];
+    p.run();
+    if (p.ghost()){
+      particles.splice(i, 1);
+    }
+  }
+  
+}
+
+function grid(){
+  randomSeed(actRandomSeed);
+  
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+
+      var posX = width / tileCount * gridX;
+      var posY = height / tileCount * gridY;
+
+     var toggle = int(random(0, 3));
+     let interA = lerpColor(col1, col2, random(1));
+     fill(interA);
+      
+      if (toggle == 0){
+        
+        beginShape();
+        vertex(posX +random(-5,5), posY+random(-5,5));
+        vertex(posX + width + random(-5,5) / tileCount, posY + random(-3,3) / tileCount);
+        vertex(posX + width / tileCount, posY + (height + random(-3,3)) / tileCount);
+        vertex(posX + random(-3,3), posY + height/tileCount);
+        endShape();
+     }
+      
+      if (toggle == 1){
+        let inter2 = lerpColor(col3, col4, random(1));
+        noFill();
+        stroke(inter2, 100, 100)
+        for (let i = 0; i < 10; i++){
+          circle(posX, posY, random(5,10)*i);
+        }
+      }
+    }
+    }
+  
+  if (frameCount%20 == 0){
+    //clear()
+    actRandomSeed = random(100000);
+    tileCount = random(2, 10);
+  }
+}
+
+class Element {
+  constructor(loc) {
+    this.accel = createVector(random(-0.05, 0.05), random(-0.05, 0.05));
+    this.vel = createVector(random(-1,1), random(-1,1));
+    this.loc = loc.copy();
+    this.lifespan = 255.0;
+    this.H1 = 100;
+  }
+
+  run(){
+    this.update();
+    this.display();
+  }
+
+  update(){
+    this.vel.add(this.accel);
+    this.loc.add(this.vel);
+    this.lifespan -= random(2);
+    this.H1 += 1;
+  }
+
+  display(){
+    noStroke();
+    rectMode(CENTER)
+    fill(this.H1, random(100), random(100), this.lifespan);
+    square(this.loc.x, this.loc.y, random(10,40));
+
+    if (this.H1 >= 200){
+      this.H1 = 150;
+    }
+
+  }
+  ghost(){
+    if (this.lifespan < 0.0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
